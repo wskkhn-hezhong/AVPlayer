931a5ccbae25b8df54a55da2d76a95af06835424
hezhong <hezhong@hzpnc.com>
2016-09-05 13:38:43 +0800

    text

---
+++ HZAVPlayer/HZAVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HZAVPlayer
+//
+//  Created by hzpnc on 16/9/5.
+//  Copyright © 2016年 hezhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
